fd24488109c277f1103d1326064291b6d411bd05
limjh16 <limjh16@gmail.com>
2020-04-23 10:32:52 +0800

    test this exclude dir method https://stackoverflow.com/questions/4210042/how-to-exclude-a-directory-in-find-command/4210072#4210072

--- scripts/jekyll.sh
+++ scripts/jekyll.sh
@@ -7,7 +7,7 @@ elif [[ ${SRC} ]]; then
   JEKYLL_SRC=${SRC}
   echo "::debug ::Using SRC environment var value $JEKYLL_SRC as a source directory"
 else
-  JEKYLL_SRC=$(find . -not \( -path vendor/bundle -prune \) -name _config.yml -exec dirname {} \;)
+  JEKYLL_SRC=$(find . -path ./vendor/bundle -prune -o -name '_config.yml' -exec dirname {} \;)
   echo "::debug ::Resolved $JEKYLL_SRC as a source directory"
 fi
 bundle exec jekyll build -s $JEKYLL_SRC -d build
